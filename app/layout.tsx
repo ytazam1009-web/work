d49edd5e5488e6cb4e5b4fc352126a9894b9d317
John <ytazam1009@gmail.com>
2026-05-13 18:41:12 +0100

    Fixed OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
         url: '/og-image.jpg',
         width: 1200,
         height: 630,
-        alt: 'Waste removal and house clearance services',
+        alt: 'Waste removal and clearance services',
       },
     ],
 
