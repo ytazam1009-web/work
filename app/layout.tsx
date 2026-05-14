57d1da3a7d8197145cac6f4e03e491f8ee18173b
John <ytazam1009@gmail.com>
2026-05-14 12:37:02 +0100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
